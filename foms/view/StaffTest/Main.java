d506a5e3e97a60f7193b1bc1e0b178893dff0a37
mengfanlei <1016231928@qq.com>
2024-03-28 14:49:34 +0800

    staff

---
+++ foms/view/StaffTest/Main.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    private static Staff[] staffMembers = {
+            new Staff("staff1", "password", 'S'),
+            new Staff("manager1", "password", 'M'),
+            new Staff("admin1", "password", 'A')
+    };
+    private static Order[] orders = {
+            new Order(1, "Item1", "New"),
+            new Order(2, "Item2", "New"),
+            new Order(3, "Item3", "Processing")
+    };
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Staff loggedInStaff = new Staff("", "", 'S');
+        // Staff actions menu
+        char choice;
+        do {
+            System.out.println("\nStaff actions:");
+            System.out.println("1. Display new orders");
+            System.out.println("2. Process order");
+            System.out.println("Enter your choice:");
+            choice = scanner.next().charAt(0);
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case '1':
+                    loggedInStaff.displayNewOrders(orders);
+                    break;
+                case '2':
+                    System.out.println("Enter order ID to process:");
+                    int orderIDToProcess = scanner.nextInt();
+                    scanner.nextLine(); // Consume newline
+                    for (Order order : orders) {
+                        if (order.getOrderID() == orderIDToProcess) {
+                            loggedInStaff.processOrder(order);
+                            break;
+                        }
+                    }
+                    break;
+                default:
+            }
+        } while (true);
+    }
+}
