c6ac71cd94a659ac650c014b746c70013ce6e18b
ziyan0117 <114735863+ziyan0117@users.noreply.github.com>
2024-04-18 13:01:16 +0800

    Update Customer.java

--- foms/models/Customer.java
+++ foms/models/Customer.java
@@ -1,10 +1,18 @@
 package foms.models;
 
-
-
+/**
+ * Represents a customer in the Food Order Management System (FOMS).
+ * Inherits from the User class and may include additional customer-specific properties and methods.
+ * 
+ * @author Chen Ziyan
+ * @version 1.0
+ * @since 2024-04-15
+ */
 public class Customer extends User{
-    // private Order order;
-
+    /**
+     * The constructor for the Customer class.
+     * Initializes a new Customer instance with a specific user type.
+     */
     public Customer() {
         super("C");
     }
